0fe7dfa9d9aaf94737c1f93f76a534a51d56e8e8
Mira <inerys.pro@gmail.com>
2024-04-20 17:59:48 +0000

    Fixed wrong escape from chroot and discord notif

--- update-nightly-base.sh
+++ update-nightly-base.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Chemin vers le dossier de construction
-BUILD_DIR="xxxx"
+BUILD_DIR="/home/almalinux/builds/base/"
 
 # Chemin vers le chroot
 CHROOT_DIR="${BUILD_DIR}"
@@ -13,18 +13,23 @@ DATE_FORMAT=$(date +"%Y%m%d%H%M")
 NEW_ARCHIVE="/var/www/html/sophora/builds/sophora-base-openrc-${DATE_FORMAT}.tar.xz"
 
 # URL du webhook Discord
-DISCORD_WEBHOOK_URL="xxxxxx"
+DISCORD_WEBHOOK_URL="xxxx"
 
 # Monter les systèmes de fichiers virtuels
-mount -t proc none "${CHROOT_DIR}/proc"
-mount -t sysfs none "${CHROOT_DIR}/sys"
+mount -t proc /proc "${CHROOT_DIR}/proc"
+mount --rbind /sys "${CHROOT_DIR}/sys"
 mount --rbind /dev "${CHROOT_DIR}/dev"
 
 # Chroot dans le dossier et exécuter les commandes de mise à jour
 chroot "${CHROOT_DIR}" /bin/bash -c "eix-sync && emerge --sync && emerge -vuDN @world && rm -rf /var/cache/distfiles/* && history -c && exit"
 
 # Sortir du chroot et démonter les systèmes de fichiers virtuels
-umount -Rl "${CHROOT_DIR}/*"
+umount -Rl "${CHROOT_DIR}dev"
+umount -Rl "${CHROOT_DIR}proc"
+umount -Rl "${CHROOT_DIR}sys"
 
 # Créer une nouvelle archive avec le nom basé sur la date
 tar -cJvf "${NEW_ARCHIVE}" -C "${BUILD_DIR}" .
+
+# Notification de création sur le serveur Discord Sophora
+curl -X POST -H "Content-Type: application/json" -d "{\"content\": \"${NEW_ARCHIVE##*/}\"}" "${DISCORD_WEBHOOK_URL}"
